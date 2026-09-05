93934578bcd68c63678f9d502f2d01d7fdf5e86a
Freddie618 <freddie.rolls@gmail.com>
2026-09-05 16:33:49 +0100

    Modify turtle drawing parameters for smoother output
    Increased the number of iterations and adjusted angles for drawing.

--- turtle.py
+++ turtle.py
@@ -3,14 +3,14 @@ import colorsys
 t = turtle.Turtle()
 s = turtle.Screen().bgcolor('black')
 t.speed(0)
-n = 70
+n = 75
 h = 0
-for i in range(360):
+for i in range(10000):
   c = colorsys.hsv_to_rgb(h,1,0.8)
   h += 1 / n
   t.color(c)
-  t.left(1)
+  t.left(0.5)
   t.fd(1)
-  for j in range (2):
-    t.left(2)
-    t.circle(100)
+  for j in range (4):
+    t.left(0.5)
+    t.circle(215)
